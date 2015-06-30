5a1be6111245bbebfbd1afb2dc679e8e7156e93e
David LeBauer <dlebauer@gmail.com>
2015-06-30 15:39:46 -0500

    wrote test for rh2qair to test equivalence with qair2rh

--- modules/data.atmosphere/tests/testthat/test.metutils.R
+++ modules/data.atmosphere/tests/testthat/test.metutils.R
@@ -7,6 +7,13 @@ test_that("sw2par, par2ppfd, sw2ppfd are consistent ",{
     expect_equal(sw2ppfd(0:1000), par2ppfd(sw2par(0:1000)))
 })
 
-test_that("qair2rh is consistent",{
-#    qair2rh(qair = 1, temp = 10, press = 1013.25)
+test_that("qair2rh and rh2qair work and are inverse",{
+    expect_equal(qair2rh(qair = 0.01, temp = 20, press = 1013.25),
+                 0.69286089020191)
+    expect_equal(qair2rh(qair = 0.001, temp = 20, press = 1013.25),
+                 0.0696648158949306)
+
+ 
+    rh2qair(rh = qair2rh(qair = 0.01, temp = 20, press = 1013.25), T = 20 + 272.15, press = 1013.25)
+                   
 })
