c2af66b6355c338c6a3dc176588640cd35322902
Betsy Cowdery <emcowdery@gmail.com>
2016-08-08 13:30:29 -0400

    The read.register function can now read model registration files as well as met

--- modules/data.atmosphere/R/read.register.R
+++ modules/data.atmosphere/R/read.register.R
@@ -16,27 +16,38 @@ read.register <- function(register.xml, con){
   register <- xmlToList(xmlParse(register.xml))
   print(as.data.frame(register))
   
-  #check scale 
-  if(is.null(register$scale)){
-    logger.error("Scale is not defined")
-  }else{
-    if(register$scale == "regional" & is.null(register$siteid)){logger.error("Region site id is not defined")}
-  }
-  
-  #check format
-  if(is.null(register$format)){ # format is not defined
-    logger.error("Format is not defined")
-  }else if(is.null(register$format$inputtype)){
-    logger.error("Browndog input type is not defined") #Ultimatly can get this from the format table in betydb
-  }else{ #format is defined 
-    if((is.null(register$format$id) & is.null(register$format$name)  & is.null(register$format$mimetype))|(is.null(register$format$id) & is.null(register$format$name))|(is.null(register$format$id) & is.null(register$format$mimetype))){
-      logger.error("Not enough format info")
-    }else if((!is.null(register$format$id) & is.null(register$format$name))| (!is.null(register$format$id) & is.null(register$format$mimetype))){
-      register$format$name <- db.query(paste("SELECT name from formats where id = ",register$format$id),con)[[1]]
-      register$format$mimetype <- db.query(paste("SELECT mime_type from formats where id = ",register$format$id),con)[[1]]
-    }else if(is.null(register$format$id) & !is.null(register$format$name)  & !is.null(register$format$mimetype)){
-      register$format$id <- db.query(paste0("SELECT id from formats where name = '",register$format$name, "' and mime_type = '", register$format$mimetype, "'"),con)[[1]]
+  if(is.null(register$type)){logger.error("Registration file type is not defined.")
+  }else if(register$type == "model"){
+    
+    if(is.null(register$update)){ 
+      logger.error("Model met update is not defined") 
+    }else{
+      invisible(register)
+      }
+    
+  }else if(register$type == "met"){
+    
+    #check scale 
+    if(is.null(register$scale)){
+      logger.error("Scale is not defined")
+    }else{
+      if(register$scale == "regional" & is.null(register$siteid)){logger.error("Region site id is not defined")}
     }
-  }
-  invisible(register)
+    
+    #check format
+    if(is.null(register$format)){ # format is not defined
+      logger.error("Format is not defined")
+    }else if(is.null(register$format$inputtype)){
+      logger.error("Browndog input type is not defined") #Ultimatly can get this from the format table in betydb
+    }else{ #format is defined 
+      if((is.null(register$format$id) & is.null(register$format$name)  & is.null(register$format$mimetype))|(is.null(register$format$id) & is.null(register$format$name))|(is.null(register$format$id) & is.null(register$format$mimetype))){
+        logger.error("Not enough format info")
+      }else if((!is.null(register$format$id) & is.null(register$format$name))| (!is.null(register$format$id) & is.null(register$format$mimetype))){
+        register$format$name <- db.query(paste("SELECT name from formats where id = ",register$format$id),con)[[1]]
+        register$format$mimetype <- db.query(paste("SELECT mime_type from formats where id = ",register$format$id),con)[[1]]
+      }else if(is.null(register$format$id) & !is.null(register$format$name)  & !is.null(register$format$mimetype)){
+        register$format$id <- db.query(paste0("SELECT id from formats where name = '",register$format$name, "' and mime_type = '", register$format$mimetype, "'"),con)[[1]]
+      }
+    }
+  } 
 }
