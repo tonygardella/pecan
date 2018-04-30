a116068cfddbd99707694f1151c7b3ce96e25599
Tony Gardella <tonygard@bu.edu>
2018-04-30 17:31:33 -0400

    add initial function info

---
+++ base/qaqc/R/del_workflow.R
@@ -0,0 +1,17 @@
+##' @export del_workflows
+##' @author Tony Gardella
+##' 
+##' @param workflow_id  Id number of workflow you want to delete 
+##' @param hostname The name of the machine you wish to delete files from 
+##' @param con connection to the bety database
+##' 
+##' @description This is a fucntion that takes in a table of records and deletes everything in the file. Please do not run this function without 
+##' 1) Backing Up Bety 
+##' 2) Checking the the file only contains entries to be deleted. 
+##'
+del_workflow<- (workflow_id,hostname,con){
+  
+  
+  
+  
+}
