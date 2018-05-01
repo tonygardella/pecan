103976f8188c8f9fcd7e7d5cfbd75e0f6b390f71
Tony Gardella <tonygard@bu.edu>
2018-05-01 18:14:31 -0400

    add information finds

--- base/qaqc/R/del_workflow.R
+++ base/qaqc/R/del_workflow.R
@@ -5,13 +5,57 @@
 ##' @param hostname The name of the machine you wish to delete files from 
 ##' @param con connection to the bety database
 ##' 
-##' @description This is a fucntion that takes in a table of records and deletes everything in the file. Please do not run this function without 
-##' 1) Backing Up Bety 
-##' 2) Checking the the file only contains entries to be deleted. 
+##' @description This is a function that takes in workflow_id and the name of the server that hosts BETY, 
+##' and then deletes all files and records associated with it.
 ##'
-del_workflow<- (workflow_id,hostname,con){
+##'
+##'wkf_id = 1000009234
+del_workflow<- (wkf_id,hostname,con){
   
+#Find workflow, ensemble,run,liklihood, input, and dbfile information
+  wkf_info <- as.data.frame(dplyr::tbl(con, "workflows") %>%
+              filter(id %in% wkf_id))
   
+  ens_info <- as.data.frame(dplyr::tbl(con, "ensembles") %>%
+                         filter(workflow_id %in% wkf_id))
   
+  run_info <- as.data.frame(dplyr::tbl(con, "runs") %>%
+                            filter(ensemble_id %in% ens_info$id))
   
+  likli_info <- as.data.frame(dplyr::tbl(con, "likelihoods") %>%
+                            filter(run_id %in% run_info$id))
+           
+  input_info <- as.data.frame(dplyr::tbl(con, "inputs") %>%
+                              filter(id %in% likli_info$input_id))
+                              
+  file_info <- as.data.frame(dplyr::tbl(con, "dbfiles") %>%
+                             filter(container_id %in% input_info$id))
+  
+# Print Info into human readable format
+  
+ model <- tbl(con, "models")%>%
+              filter(id %in% wkf_info$model_id) %>%
+              select(model_name)
+ 
+ type <- ens_info$runtype
+ 
+ site <-  as.data.frame(tbltbl(con,"sites")%>%
+                        filter(id %in% wkf_info$site_id)%>%
+                        select_('sitename'))
+ 
+ user_name <- as.data.frame(tbl)
+ 
+ glue('You are deleting a workflow executed by {user_name}.')
+ 
+ 
+ ## Pause for 15 sec so person can stop execution if needed
+ PEcAn.logger::logger.warn("Deletion will begin in 15 seconds")
+ profvis::pause(15)
+ PEcAn.logger::logger.warn("Deletion of records and files begginning now")
+ 
+ ## Delete From the bottom up
+ 
+ if
+ 
+ 
 }
